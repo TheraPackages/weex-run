95f5ec4c13e201e3f8b4d5e776100c361550a19c
scucheri <cheri.xxy@alibaba-inc.com>
2017-04-17 17:12:04 +0800

    add device panel

--- index.js
+++ index.js
@@ -4,14 +4,19 @@
 
 const WeexRuner = require('./lib/weex-run')
 
+const DevicePanel = require('./lib/thera-device-panel')
+
+
 module.exports = {
 
   // start server dumplings
   activate () {
     this.runner = new WeexRuner()
+    this.devicePanel = new DevicePanel();
+    this.devicePanel.activate();
   },
 
   deactivate () {
-
+    this.devicePanel.deativate();
   }
 }
